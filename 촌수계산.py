75682eaa211f7a767b57c92fec017d5802303baa
ahnjungeun <616a65@gmail.com>
2023-09-12 13:58:11 +0900

    풀었어용~!~!

--- 촌수계산.py
+++ 촌수계산.py
@@ -11,26 +11,17 @@ for _ in range(int(input())):
 m = [0]*n
 
 def bfs(G,v,m):
-    global b
     Q = [v]
     m[v] = 1
-    c=0
     while Q:
-        # c+=1
         p = Q.pop(0)
-        print('p,c',p,c)
         for i in G[p]:
-            
             if not m[i]:
                 Q.append(i)
-                m[i] = 1
+                m[i] = m[p]+1
 
-            if i == b:
-                return(c)
-        c+=1
-
-r = bfs(G,a,m)
-print(r if r else -1)
+bfs(G,a,m)
+print(m[b]-1)
 
 # https://www.acmicpc.net/board/view/64738
 # 반례
